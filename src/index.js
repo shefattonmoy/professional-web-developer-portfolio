9a24410f57a501e2cfed6914ffd5dea6e39ae66d
shefattonmoy <shefathossain7@gmail.com>
2025-07-08 15:12:43 +0600

    updated

--- src/index.js
+++ src/index.js
@@ -3,9 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+document.addEventListener('DOMContentLoaded', () => {
+  const rootElement = document.getElementById('root');
+  console.log('Root element found:', rootElement);
+  if (rootElement) {
+    const root = ReactDOM.createRoot(rootElement);
+    root.render(
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    );
+  } else {
+    console.error('❌ Root element not found in DOM!');
+  }
+});
